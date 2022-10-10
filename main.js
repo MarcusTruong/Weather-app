9b6820367a532e0a6a141cff280ed66c6d4223c5
Marcus Truong <truong22082002@gmail.com>
2022-10-10 10:58:14 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let weather = {
+    'apiKey' : "6f24a74f426c2417daa6e913b6b5f66e",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&APPID=6f24a74f426c2417daa6e913b6b5f66e")
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data)
+    )},
+    displayWeather: function (data) {
+         const { name } = data ;
+         const { icon, description } = data.weather[0];
+         const { temp , humidity } = data.main ; 
+         const { speed } = data.wind ; 
+         document.querySelector('.city').innerHTML = `Weather in ${name}`;
+         document.querySelector('.temp').innerHTML = `${temp}°C`;
+         document.querySelector('.icon').src = "https://openweathermap.org/img/wn/" +icon+".png";
+         document.querySelector('.description').innerHTML = description;
+         document.querySelector('.humidity').innerHTML = `Humidity: ${humidity}%`;      
+         document.querySelector('.wind').innerHTML =`Wind speed: ${speed} km/h` ;
+         document.querySelector('.weather').classList.remove('loading');
+         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+name+ "' )"
+    },
+    search : function () {
+        this.fetchWeather(document.querySelector('.search-bar').value)
+    }
+}
+document.querySelector('.search button').addEventListener('click', function (){
+     weather.search();
+})
+document.querySelector('.search-bar').addEventListener('keyup',function (e) {
+    if(e.key == 'Enter') {
+        weather.search();
+    }
+})
+
+// console.log(document.querySelector('.city').innerHTML)
+console.log(document.querySelector('.city').innerText)
+weather.fetchWeather('ha noi')
